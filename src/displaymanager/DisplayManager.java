3c3ee213ca3526b2e56dbc13988e5678d12978b4
Pievi <Pievi@DESKTOP-V8AHUP7>
2017-05-07 19:52:27 +0300

    json reading, display loop and delays

--- src/displaymanager/DisplayManager.java
+++ src/displaymanager/DisplayManager.java
@@ -9,6 +9,7 @@ import java.awt.FlowLayout;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -21,23 +22,50 @@ import javax.swing.JLabel;
 public class DisplayManager {
     public static void main(String avg[]) throws IOException
     {
-        DisplayManager abc=new DisplayManager();
+//        DisplayManager abc = new DisplayManager();
     }
 
-    public DisplayManager() throws IOException
+    public DisplayManager(ArrayList configuers) throws IOException, InterruptedException
     {
-        System.out.println("here");
-        BufferedImage img=ImageIO.read(new File("CurrentData/ps1_emu_v1.png"));
-        ImageIcon icon=new ImageIcon(img);
-        JFrame frame=new JFrame();
-        frame.setLayout(new FlowLayout());
-        frame.setSize(200,300);
-        JLabel lbl=new JLabel();
-        lbl.setIcon(icon);
-        frame.add(lbl);
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
-        frame.setUndecorated(true);
-        frame.setVisible(true);        
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
+        
+        Boolean start = false;
+        Integer adCounter = 0;
+        Integer taimdfe = 1000;
+        String time = "";
+        
+        
+        while(!start) { 
+            
+            ArrayList conf = (ArrayList) configuers.get(adCounter);
+            time = (String) conf.get(1);
+            String file = (String) conf.get(0);
+            
+            System.out.println("here");
+            BufferedImage img=ImageIO.read(new File("CurrentData/"+file));
+            ImageIcon icon=new ImageIcon(img);
+            JFrame frame=new JFrame();
+            frame.setLayout(new FlowLayout());
+            frame.setSize(200,300);
+            JLabel lbl=new JLabel();
+            lbl.setIcon(icon);
+            frame.add(lbl);
+            frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+            frame.setUndecorated(true);
+            frame.setVisible(true);        
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            
+           
+            
+            
+            Thread.sleep(Integer.parseInt(time));
+            
+            if((adCounter+1) >= configuers.size()){
+                adCounter = 0;
+            }else{
+                adCounter++;
+            }
+        }
     }
 }
