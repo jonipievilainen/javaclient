0d5f849c46b55bb788e5accdf363f69b1b4f6ab5
Pievi <Pievi@DESKTOP-V8AHUP7>
2017-07-26 19:44:34 +0300

    added config file check

--- src/clienttidemo/ClienttiDemo.java
+++ src/clienttidemo/ClienttiDemo.java
@@ -9,6 +9,13 @@ import java.io.*;
 import java.net.*;
 import devicemanager.DeviceManager;
 import filemanager.FileManager;
+import java.util.Scanner;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Properties;
+
 
 /**
  *
@@ -21,10 +28,59 @@ public class ClienttiDemo {
      */
     public static void main(String[] args) throws Exception {
         
+        File f = new File("config.properties");
+        Properties prop = new Properties();
+        
+        if(f.exists() && !f.isDirectory()) { 
+            
+            InputStream input = null;
+            
+            try {
+                input = new FileInputStream("config.properties");
+                prop.load(input);
+                System.out.println(prop.getProperty("deviceName"));
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            } finally {
+                if (input != null) {
+                    try {
+                        input.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }else{
+            Scanner scanner = new Scanner(System.in);
+            String deviceName;
+            
+            System.out.println("Enter device name:");
+            deviceName=scanner.nextLine();
+            
+            OutputStream output = null;
+            
+            try {
+                output = new FileOutputStream("config.properties");
+                prop.setProperty("deviceName", deviceName); // dublicate if you need more´settings
+                // save properties to project root folder
+                prop.store(output, null);
+            } catch (IOException io) {
+                io.printStackTrace();
+            } finally {
+                if (output != null) {
+                    try {
+                        output.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        
         devicemanager.DeviceManager thisDevice = new devicemanager.DeviceManager();
         filemanager.FileManager currrentFiles = new filemanager.FileManager();
         
-        thisDevice.getEthernetMacNumber();
+        // thisDevice.getEthernetMacNumber();
         
         currrentFiles.checkCurrentFilesSameAsServer();
         
